8829d2774f82211da2f0d222c169dd0212615c56
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-05-22 17:32:36 -0300

    Requisito 3 e 5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import validator from 'validator';
+import PropTypes from 'prop-types';
+import { loginPlayer } from '../redux/reducers/actions';
 
 class Login extends React.Component {
   state = {
@@ -23,11 +25,15 @@ class Login extends React.Component {
     });
   };
 
-  //   login = () => {
-  //   };
+  login = () => {
+    const { history, dispatch } = this.props;
+    dispatch(loginPlayer(this.state));
+    history.push('/game');
+  };
 
   render() {
     const { email, name, isDisabled } = this.state;
+    const { history } = this.props;
     return (
       <div>
         <div>
@@ -60,6 +66,12 @@ class Login extends React.Component {
           >
             Play
           </button>
+          <button
+            data-testid="btn-settings"
+            onClick={ () => { history.push('/settings'); } }
+          >
+            Menu de configurações
+          </button>
         </div>
       </div>
     );
@@ -67,3 +79,9 @@ class Login extends React.Component {
 }
 
 export default connect()(Login);
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
---
+++ src/redux/reducers/actions/index.js
@@ -0,0 +1,6 @@
+export const LOGIN_PLAYER = 'LOGIN_PLAYER';
+
+export const loginPlayer = (state) => ({
+  type: LOGIN_PLAYER,
+  payload: state,
+});
