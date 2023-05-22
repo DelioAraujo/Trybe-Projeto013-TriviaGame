87277a1b995fbe3c51a9e7c2de7c226767c76675
Amanayara Slot <amanayaradev@gmail.com>
2023-05-22 16:04:43 -0300

    requisito um

---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import user from './user';
+
+const rootReducer = combineReducers({ user });
+
+export default rootReducer;
+
+// Configure os seus reducers.
+// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
