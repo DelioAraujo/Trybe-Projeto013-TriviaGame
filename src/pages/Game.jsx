69391e25a57f77b0d9a727c89d3442261c8c9f7f
DelioAraujo <delioaraujo@gmail.com>
2023-05-22 18:30:38 -0300

    req 8 incompleto

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -4,10 +4,45 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Game extends Component {
+  state = {
+    time: 30,
+    timeOutID: null,
+    respondeu: false,
+    resposta:''
+  };
+
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  startTimer = () => {
+    const seconds = 1000;
+    const timeOutID = setInterval(() => {
+      const { time } = this.state;
+      if (time > 0) {
+        this.setState((prevState) => ({ time: prevState.time - 1 }));
+      } else if (time === 0) {
+        this.stopTimer();
+        this.setState({
+          resposta: 'errada'
+        });
+      }
+    }, seconds);
+    this.setState({ timeOutID });
+  };
+
+  stopTimer = () => {
+    const { timeOutID } = this.state;
+    clearInterval(timeOutID);
+  };
+
   render() {
     const { state: { name, email, score } } = this.props;
+    const { time } = this.state;
+
     return (
       <div>
+        <div>{time}</div>
         <h2 data-testid="header-player-name">{ name }</h2>
         <img
           data-testid="header-profile-picture"
