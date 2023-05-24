fd63d7abf4e567818a23828e6c85f6b33989cced
andrexster <zoio1@hotmail.com>
2023-05-24 17:49:03 -0300

    Resolvendo conflito

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  state = {
+    messageFeedback: '',
+  };
+
+  componentDidMount() {
+    this.message();
+  }
+
+  message = () => {
+    const {
+      state: { assertions },
+    } = this.props;
+    const TRES = 3;
+
+    console.log(assertions);
+    if (assertions < TRES) {
+      this.setState({
+        messageFeedback: 'Could be better...',
+      });
+    } else if (assertions >= TRES) {
+      this.setState({
+        messageFeedback: 'Well Done!',
+      });
+    }
+  };
+
   render() {
     const { state: { score, assertions } } = this.props;
+    const { messageFeedback } = this.state;
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
         <Header />
-        <p data-testid="feedback-total-score">
-          {score}
-        </p>
-        <p data-testid="feedback-total-question">
-          {assertions}
-        </p>
+        <div data-testid="feedback-text">{messageFeedback}</div>
+        <p data-testid="feedback-total-score">{score}</p>
+        <p data-testid="feedback-total-question">{assertions}</p>
       </div>
     );
   }
@@ -31,5 +55,4 @@ Feedback.propTypes = {
     assertions: PropTypes.number,
   }),
 }.isRequired;
-
 export default connect(mapStateToProps)(Feedback);
