6bf86c1219939049178678af06937e18a90685e5
DelioAraujo <delioaraujo@gmail.com>
2023-05-24 17:41:24 -0300

    16 e 18 passando

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -6,6 +6,7 @@ import Header from '../components/Header';
 class Feedback extends React.Component {
   render() {
     const { state: { score, assertions } } = this.props;
+    const { history } = this.props;
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
@@ -16,6 +17,12 @@ class Feedback extends React.Component {
         <p data-testid="feedback-total-question">
           {assertions}
         </p>
+        <button
+          data-testid="btn-ranking"
+          onClick={ () => history.push('/ranking') }
+        >
+          Ranking
+        </button>
       </div>
     );
   }
